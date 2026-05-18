92f4263101aaf0ea49c1bed228819628bab99940
18millo <levinjenga097@gmail.com>
2026-05-18 12:25:01 +0300

    Add Cloudinary media storage. Migrate 49 photos + profile pics to cloud

--- photo_gallery_project/settings.py
+++ photo_gallery_project/settings.py
@@ -17,6 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
+    'cloudinary_storage',
     'photo_gallery',
 ]
 
@@ -74,8 +76,15 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', ''),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY', ''),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET', ''),
+}
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
